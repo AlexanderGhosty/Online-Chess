0f4fddc4bc33296276ef687513fd55f32ef933fe
AlexanderGhosty <a.z.bykov@stud.mospolytech.ru>
2023-04-08 21:19:13 +0300

    Server VS project was created

---
+++ Server/Server/Server/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <atomic>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+int main()
+{
+	std::cout << "Hello Server";
+	return 0;
+}
